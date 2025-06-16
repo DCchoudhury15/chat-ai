cf5ca5494305cf6f9411b8cda8989339c481dd3d
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-06-16 23:52:03 +0530

    agents-datatable

--- src/modules/auth/ui/views/agents/server/procedure.ts
+++ src/modules/auth/ui/views/agents/server/procedure.ts
@@ -3,7 +3,7 @@ import { agents } from "@/db/schema";
 import { db } from "@/db"
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 
 
@@ -11,7 +11,12 @@ export const agentsRouter =createTRPCRouter({
 
    getOne:protectedProcedure.input(z.object({id : z.string()})).query(async({input})=>{
         const [existingAgent] =await db
-        .select()
+        .select({
+            meetingCount:sql<number>`5`,
+            ...getTableColumns(agents),
+           
+
+   })
         .from(agents)
         .where (eq(agents.id, input.id))
 
