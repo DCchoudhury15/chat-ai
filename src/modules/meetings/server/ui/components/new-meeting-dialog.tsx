d5dfaba85b2f254ed0ab8e1455a10107e4f2fb45
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-06-18 18:16:15 +0530

    meetings-form

---
+++ src/modules/meetings/server/ui/components/new-meeting-dialog.tsx
@@ -0,0 +1,33 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { MeetingFrom } from "./meeting-form";
+import { useRouter } from "next/navigation";
+
+interface NewMeetingDialogProps{
+    open: boolean;
+    onOpenChange:(open:boolean)=> void;
+};
+
+export const NewMeetingDialog=({
+    open,
+    onOpenChange,
+}:NewMeetingDialogProps)=>{
+    const router = useRouter();
+    return(
+        <ResponsiveDialog
+        title="New Meeting"
+        description="Create a new meeting"
+        open={open}
+        onOpenChange={onOpenChange}
+        >
+   
+<MeetingFrom
+            onSuccess={(id) => {
+                onOpenChange(false);
+                router.push(`/meetings/${id}`);
+            }}
+            onCancel={() => 
+                onOpenChange
+            }/>
+        </ResponsiveDialog>
+    )
+}
